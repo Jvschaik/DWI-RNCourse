318d561478ac36eafe9e30ffe97268e7e52830f4
Jvschaik <jsonschaik@gmail.com>
2020-10-02 08:40:53 +0200

    RNCourse setup + screens

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+import AppPicker from "./app/components/AppPicker";
+import AppTextInput from "./app/components/AppTextInput";
+import LoginScreen from "./app/screens/LoginScreen";
+
+export default function App() {
+  
+  return (
+    <LoginScreen />
+  );
+}
