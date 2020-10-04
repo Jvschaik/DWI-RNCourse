3a66fd284621de69dba8dde53c396bdd7a10bee3
Jvschaik <jsonschaik@gmail.com>
2020-10-04 08:51:59 +0200

    Listitem chevron

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, {useState} from "react";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
   
   return (
-    <ListingEditScreen />
+    <ListingDetailsScreen />
   );
 }
