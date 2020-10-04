7098cef2a1619a4cb34d6dd229fac214d521b174
Jvschaik <jsonschaik@gmail.com>
2020-10-04 08:58:33 +0200

    fixing number of lines

--- app/components/Card.js
+++ app/components/Card.js
@@ -8,8 +8,8 @@ function Card({ title, subTitle, image }) {
         <View style={styles.card}>
             <Image style={styles.image} source={image} />
             <View style={styles.detailsContainer}>
-                <AppText style={styles.title}>{title}</AppText>
-                <AppText style={styles.subtitle}>{subTitle}</AppText>
+                <AppText style={styles.title} numberOfLines={1}>{title}</AppText>
+                <AppText style={styles.subtitle} numberOfLines={2}>{subTitle}</AppText>
             </View>
         </View>
     );
