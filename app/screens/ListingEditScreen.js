8fe3deb529c7cfc73cd20dca8f8c7436057ddf91
Jvschaik <jsonschaik@gmail.com>
2020-10-07 09:59:23 +0200

    get location

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet } from "react-native";
 import * as Yup from "yup";
-import CategoryPickerItem from "../components/CategoryPickerItem";
+import * as Location from 'expo-location';
 
 import {
   AppForm as Form,
@@ -9,6 +9,7 @@ import {
   AppFormPicker as Picker,
   SubmitButton,
 } from "../components/forms";
+import CategoryPickerItem from "../components/CategoryPickerItem";
 import FormImagePicker from "../components/forms/FormImagePicker";
 import Screen from "../components/Screen";
 
@@ -58,6 +59,19 @@ const categories = [
 ];
 
 function ListingEditScreen() {
+  const [location, setLocation] =  useState();
+
+  const getLocation = async () => {
+    const { granted } = await Location.requestPermissionsAsync();
+    if (!granted) return;
+    const { coords: { latitude, longitude }} = await Location.getLastKnownPositionAsync();
+    setLocation({latitude, longitude});
+  }
+
+  useEffect(() => {
+    getLocation();
+  }, [])
+
   return (
     <Screen style={styles.container}>
       <Form
@@ -68,7 +82,7 @@ function ListingEditScreen() {
           category: null,
           images: []
         }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={(values) => console.log(location)}
         validationSchema={validationSchema}
       >
         <FormImagePicker name="images"/>
